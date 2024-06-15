6a7ba4f362fa45d50122b259871d8507d3c96987
Sam123000 <soon.h.ng@outlook.com>
2024-06-15 22:47:27 +1200

    Made a map on the detail page

---
+++ app_1.py
@@ -0,0 +1,32 @@
+from flask import Flask
+
+app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Drink(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80), unique=True, nullable=False)
+	description = db.Column(db.String(120))
+
+	def __repr__(self):
+		return f"{self.name} - {self.description}"
+
+@app.route('/')
+def index():
+    return "Hello!"
+
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+
+output = []
+    for drink in drinks:
+        drink_date = {'name': drink.name, 'description':drink.description}
+        output.append(drink_data)
+            return {"drinks":output}
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
